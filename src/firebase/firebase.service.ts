6778474cbf63f715483e1f578ebeb466e2afba07
Naim Shihab <92425371+naimshihab21@users.noreply.github.com>
2024-08-14 02:39:51 +0800

    add

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,22 +1,18 @@
 import * as admin from 'firebase-admin';
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { FB_SERVICE } from './service';
 
 @Injectable()
 export class FirebaseService implements OnApplicationBootstrap {
   private firebaseApp: admin.app.App;
 
-  constructor(private readonly configService: ConfigService) {}
-
   async onApplicationBootstrap() {
     if (admin.apps.length === 0) {
       this.firebaseApp = admin.initializeApp({
         credential: admin.credential.cert({
-          clientEmail: this.configService.get<string>('CLIENT_EMAIL'),
-          privateKey: this.configService
-            .get<string>('PRIVATE_KEY')
-            .replace(/\\n/g, '\n'),
-          projectId: this.configService.get<string>('PROJECT_ID'),
+          clientEmail: FB_SERVICE.client_email,
+          privateKey: FB_SERVICE.private_key,
+          projectId: FB_SERVICE.project_id,
         }),
       });
     } else {
